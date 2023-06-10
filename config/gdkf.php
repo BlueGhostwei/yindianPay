a89ba5fd79bdd9c8e51d0302e9d4ad0e12a34ad3
huangwei <13226431320@163.com>
2023-06-10 11:54:48 +0800

    修改配置文件

--- config/gdkf.php
+++ config/gdkf.php
@@ -2,13 +2,15 @@
 
 return [
     'config'=>[
+        //商户号
+        'mchtCd'=>'',
         //机构秘钥
-        'secretKey'=>'WJ6X1X6IRMO2EXZYLYWWZGMY',
+        'secretKey'=>'',
         //秘钥类型域，机构秘钥:ORG、商户秘钥：MCHT、终端秘钥：TERM
         'typeField'=>'ORG',
         //orgCd是机构号
-        'orgCd'=>'201901176661391',
+        'orgCd'=>'',
         //请求地址，区分测试与生产
-        'reqUrl'=>'http://test.api.route.hangmuxitong.com',
+        'reqUrl'=>'',
     ]
 ];
